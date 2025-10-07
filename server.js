9717b056345ef8dd8d88d979c0ab26e7122a1256
kezapa <kezia@kabarac.ac.ke>
2025-10-08 02:48:51 +0300

    Fix Express routing compatibility

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 10000;
 
+const distPath = path.join(__dirname, 'dist/RentEase/browser');
+
 // Serve static files from Angular build
-app.use(express.static(path.join(__dirname, 'dist/RentEase/browser')));
+app.use(express.static(distPath));
 
 // Handle Angular routing - send all requests to index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/RentEase/browser/index.html'));
+app.use((req, res) => {
+  res.sendFile(path.join(distPath, 'index.html'));
 });
 
 app.listen(port, '0.0.0.0', () => {
