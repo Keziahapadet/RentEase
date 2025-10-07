cd0c163651ab638111d759a67db38a3a55c91a6b
kezapa <kezia@kabarac.ac.ke>
2025-10-08 02:45:02 +0300

    Fix Express route pattern

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 10000;
 app.use(express.static(path.join(__dirname, 'dist/RentEase/browser')));
 
 // Handle Angular routing - send all requests to index.html
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/RentEase/browser/index.html'));
 });
 
